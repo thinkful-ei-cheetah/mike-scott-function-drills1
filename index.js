90537500ce2dc175dc5c696be6249785520c1049
Scott Dowding <scottddowding1279@gmail.com>
2019-03-25 14:39:48 -0400

    added try catch block for negative age check

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 function getYearOfBirth(age) {
+  if (age < 0) {
+    throw new Error('Age cannot be negative');
+  }
   return 2019 - age;
 }
 
@@ -8,5 +11,10 @@ function createGreeting(name, age) {
   return `Hi, my name is ${name}, and I am ${age} years old. I was born in ${yob}`;
 }
 
-const greeting1 = createGreeting('Scott', 39);
-console.log(greeting1);
+try {
+  const greeting1 = createGreeting('Scott', 39);
+  console.log(greeting1);
+} catch(e) {
+  console.log(e.message);
+}
+

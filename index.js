8065cef4f8f1625732edbbffdb069331eff30017
Scott Dowding <scottddowding1279@gmail.com>
2019-03-25 14:52:41 -0400

    check for valid argument types

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ function createGreeting(name, age) {
   if (!name || !age) {
     throw new Error('Arguments not valid.');
   }
+  if (typeof name !== 'string' || typeof age !== 'number') {
+    throw new TypeError('Arguments have wrong type');
+  }
   const yob = getYearOfBirth(age);
   return `Hi, my name is ${name}, and I am ${age} years old. I was born in ${yob}`;
 }
 
 try {
-  const greeting1 = createGreeting('Scott', 39);
+  const greeting1 = createGreeting('39', '39');
   console.log(greeting1);
 } catch(e) {
   console.log(e.message);

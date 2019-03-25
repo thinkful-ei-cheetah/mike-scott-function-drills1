299800e8414f5172292856ed49d52e74400613cb
MikeJung90 <kpnba90@gmail.com>
2019-03-25 14:35:18 -0400

    made a function with year of birth

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
+function getYearOfBirth(age) {
+  return 2019 - age;
+}
+
 function createGreeting(name, age) {
-  const yearOfBirth = 2019 - age;
-  return `Hi, my name is ${name}, and I am ${age} years old. I was born in ${yearOfBirth}`;
+  const yob = getYearOfBirth(age);
+  return `Hi, my name is ${name}, and I am ${age} years old. I was born in ${yob}`;
 }
 
 const greeting1 = createGreeting('Scott', 39);
